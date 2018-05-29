9c8ebc564319809f24ee4318c8b4ad81dd91bccf
Boyko Amarov <boyko.amarov@gmail.com>
2018-05-29 12:46:35 +0300

    Create Problem_2.R

---
+++ Problem_2.R
@@ -0,0 +1,13 @@
+## Homework 4, Problem 2
+crime <- read.delim('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/crime.csv', stringsAsFactors = FALSE)
+str(crime)
+
+## a)
+
+## b)
+
+## c)
+
+## d)
+
+## e)
