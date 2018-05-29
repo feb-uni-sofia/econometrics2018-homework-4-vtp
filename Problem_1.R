232f86f62d53f25160af1b69456ee2e7b2f5b7dd
Boyko Amarov <boyko.amarov@gmail.com>
2018-05-29 12:46:11 +0300

    Create Problem_1.R

---
+++ Problem_1.R
@@ -0,0 +1,25 @@
+## Homework 4, Problem 1
+
+library(dplyr)
+
+## Read the data
+houseWork <- read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/houseWork.csv')
+str(houseWork)
+
+## a)
+
+## b)
+
+## c)
+
+## d)
+
+## e)
+
+## f)
+
+## g)
+
+## h)
+
+## i)
