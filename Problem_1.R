ab4d5510936a19b5cc2aecd3163b6bcca15e5062
Tanya Rabadjiyska <t.rabadjiyska@yahoo.com>
2018-06-12 21:05:12 +0300

    HW4

--- Problem_1.R
+++ Problem_1.R
@@ -7,21 +7,42 @@ houseWork <- read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/da
 str(houseWork)
 
 ## a)
+table(houseWork$sex)
 
 ## b)
+HoursMale <- houseWork[houseWork$sex == 'm', 'hours']
+mean(HoursMale)
+
+HoursFemale <- houseWork[houseWork$sex == 'f', 'hours']
+mean(HoursFemale)
 
 ## c)
+houseWork <- within(houseWork, { 
+  female <- ifelse(houseWork$sex == 'f', 'TRUE', 'FALSE')
+  male <- ifelse(houseWork$sex == 'm', 'TRUE', 'FALSE')
+})
 
 ## d)
+fit <- lm(hours ~ female, data = houseWork)
+summary(fit)
 
-## e)
+## e) The coefficient beta0 shows the time needed for house work 
+## independently from the number of females added. While beta1 shows
+## how much the hours for house work for each subsequent women are decreasing.
 
 ## f)
+t.test(HoursFemale, HoursMale, alternative = 'greater')
 
 ## g)
+pt(-14.94149, df = 11016 - 2)
 
-## h)
+## h) No, because the p-value is greater than 0.05.
 
-## i)
+## i) Central Limit Theorem and Normal Distribution are the distributional 
+## assumptions of the test and justify the confidential interval.
 
 ## j)
+fitAll <- lm(hours ~ female + male, data = houseWork)
+summary(fitAll)
+
+## There is no relationship between the variables.
--- Problem_2.R
+++ Problem_2.R
@@ -3,11 +3,22 @@ crime <- read.delim('https://s3.eu-central-1.amazonaws.com/econometrics2018/data
 str(crime)
 
 ## a)
+fit <- lm(C ~ HS, data = crime)
+summary(fit)
 
 ## b)
-
+  
 ## c)
+fit1 <- lm(C ~ HS + U, data = crime)
+summary(fit1)
 
-## d)
+## Tested apart from the other factors, HS has a positive contribution to
+## the crime rate, but when we add percent of urban, the relationship between
+## HS and the crime rate becomes negative.(Simpson's Paradox)
+
+## d) They are wrong, because the education does not increase the crime rates.
+## The increase in crime rates is a consequences of the urbanization, which has
+## positive contribution on them. But also educated people live mainly in the cities,
+## and the positive contribution from (1) includes the urban influence.
 
 ## e)
